581a0861f1f9bee5ce340784aa5974ee2b4ea6a7
William Trindade <williamtrindade777@gmail.com>
2021-06-01 20:29:38 -0300

    Update answers.sql

--- exercises/class01/answers.sql
+++ exercises/class01/answers.sql
@@ -1,3 +1,7 @@
 -- 1
 SELECT atleta.nomat, patrocinador.nompat FROM atleta, patrocinador, atleta_pat 
 WHERE atleta_pat.codat = atleta.codat AND atleta_pat.codpat = patrocinador.codpat;
+
+-- 2
+SELECT atleta.nomat, estado.nomeuf, patrocinador.nompat FROM atleta, patrocinador, atleta_pat, estado
+WHERE atleta_pat.codat = atleta.codat AND atleta_pat.codpat = patrocinador.codpat AND atleta.siglauf = estado.siglauf;
