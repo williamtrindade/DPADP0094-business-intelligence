12234ba4e041061d63568c420f7d51f289d325a8
William Trindade <williamtrindade777@gmail.com>
2021-06-01 20:39:34 -0300

    Update answers.sql

--- exercises/class01/answers.sql
+++ exercises/class01/answers.sql
@@ -5,3 +5,8 @@ WHERE atleta_pat.codat = atleta.codat AND atleta_pat.codpat = patrocinador.codpa
 -- 2
 SELECT atleta.nomat, estado.nomeuf, patrocinador.nompat FROM atleta, patrocinador, atleta_pat, estado
 WHERE atleta_pat.codat = atleta.codat AND atleta_pat.codpat = patrocinador.codpat AND atleta.siglauf = estado.siglauf;
+
+-- 3
+SELECT atleta.nomat, COUNT(atleta_pat.codpat) FROM atleta, patrocinador, atleta_pat
+WHERE atleta_pat.codat = atleta.codat AND atleta_pat.codpat = patrocinador.codpat
+GROUP BY atleta.nomat;
