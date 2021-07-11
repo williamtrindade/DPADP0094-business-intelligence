79505002caa258bfbb7849653b3572a644adde39
William Trindade <williamtrindade777@gmail.com>
2021-07-11 15:06:54 -0300

    Update answers.sql

--- exercises/class02/answers.sql
+++ exercises/class02/answers.sql
@@ -4,3 +4,12 @@ FROM raca
 JOIN cavalo ON cavalo.codraca = raca.codraca
 JOIN criador on cavalo.codcriador = criador.codcriador
 GROUP BY GROUPING SETS ((raca.nomeraca), (criador.nomecriador), ());
+
+-- 2
+SELECT criador.nomecriador criador, count(cavalo.codcavalo) "qtd cavalo", pais.nomepais pais
+FROM criador
+JOIN cavalo ON cavalo.codcriador = criador.codcriador
+JOIN pais on pais.codpais = criador.codpais
+GROUP BY GROUPING SETS ((criador.nomecriador, pais.nomepais), (pais.nomepais), ());
+
+-- 3
